549b48ffe77e629cb320e4fda5ffcba740554a6e
Dustin <clerin+gitkraken@gmail.com>
2019-11-13 16:29:43 -0800

    added lecture version to lru_cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -10,8 +10,12 @@ class LRUCache:
     """
     def __init__(self, limit=10):
         self.limit = limit
-        self.storage = DoublyLinkedList()
-        self.cache = {}
+        # self.storage = DoublyLinkedList()
+        # self.cache = {}
+        # LECTURE VERSION
+        self.size = 0
+        self.storage = dict()
+        self.order = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -21,9 +25,17 @@ class LRUCache:
     the key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        if key in self.cache:
-            self.storage.move_to_front(self.cache[key])
-            return self.cache[key].value
+        # if key in self.cache:
+        #     # self.storage.move_to_front(self.cache[key])
+        #     self.storage.move_to_end(self.cache[key])
+        #     return self.cache[key].value
+        # LECTURE VERSION
+        if key in self.storage:
+            node = self.storage[key]
+            self.order.move_to_end(node)
+            return node.value[1]
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-added pair
@@ -34,22 +46,60 @@ class LRUCache:
     we simply want to overwrite the old value associated with the key
     with the newly-specified value.
     """
-    def set(self, key, value):
-        if key in self.cache:
-            self.storage.move_to_front(self.cache[key])
-            if self.cache[key].value != value:
-                self.cache[key].value = value
-        else:
-            if len(self.cache) == self.limit:
-                el = self.storage.tail
-                del_key = ''
-                for the_key in self.cache:
-                    if self.cache[the_key].value == el.value:
-                        del_key = the_key
-                self.cache.pop(del_key)
-                self.storage.delete(el)
-            self.storage.add_to_head(value)
-            self.cache[key] = self.storage.head
+    # def set(self, key, value):
+    #     if key in self.cache:
+    #         # self.storage.move_to_front(self.cache[key])
+    #         self.storage.move_to_end(self.cache[key])
+    #         if self.cache[key].value != value:
+    #             self.cache[key].value = value
+    #     else:
+    #         if len(self.cache) == self.limit: # {"item1":node} --- node.value == "1"
+    #             # el = self.storage.tail
+    #             el = self.storage.head
+    #             del_key = ''
+    #             for the_key in self.cache:
+    #                 if self.cache[the_key].value == el.value:
+    #                     del_key = the_key
+    #                     return
+    #             self.cache.pop(del_key)
+    #             self.storage.delete(el)
+    #         # self.storage.add_to_head(value)
+    #         # self.cache[key] = self.storage.head
+    #         self.storage.add_to_tail(value)
+    #         self.cache[key] = self.storage.tail
+
+    # LECTURE VERSION
+    def set(self, key, value): # {"item1":node} ---- node.value == (item1, 1)
+        # cases to handle
+        # does the key already exist in the cache
+        if key in self.storage:
+            # key is here so we should replace the value
+            node = self.storage[key]
+            node.value = (key, value)
+            self.order.move_to_end(node)
+            return
+        # are we at cap or not?
+        # yes - dump oldest item
+        if self.size == self.limit:
+            # dump the oldest item
+            # remove from head/order
+            # delete the key/value pair
+            # option 1, explicit
+            # del self.storage[self.order.head.value[0]]
+            # self.order.remove_from_head()
+
+            # option 2
+            del self.storage[self.order.remove_from_head()[0]]
+            # subtract count
+            self.size -= 1
+        
+        # no - nothing extra needed
+
+        # how do we put stuff into the cache?
+        #if cache not full and key not present
+        self.order.add_to_tail((key, value))
+        self.storage[key] = self.order.tail
+        self.size += 1
 
 
 # l = LRUCache(3)
